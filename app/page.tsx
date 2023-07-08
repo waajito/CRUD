30e6e9628a7318fa96827151e62b095b1edad7d5
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2023-07-09 01:57:53 +0530

    added a article form

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <form action="" className={styles.createArticle}>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" />
+        <label htmlFor="content">Content</label>
+        <textarea name="content" id="content" cols={30} rows={10}></textarea>
+        <button type="submit" className={styles.button}>
+          Submit
+        </button>
+      </form>
+    </main>
+  );
 }
